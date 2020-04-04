fcfba1600cbaf5586179ee93f52e4f74e324e02c
Josef Gullström <josef@pool.fi>
2020-04-04 23:58:41 +0300

    FieldArray example

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 
-import { Form, Input } from 'react-hook-smartform';
-import 'react-hook-smartform/dist/index.css';
+import {
+  Form, Input, FormScope, useFieldArrayScope, FieldArray,
+} from 'react-hook-smartform';
+
+const Addresses = () => {
+  const { renderFields, append, remove } = useFieldArrayScope();
+
+  return (
+    <div>
+      Addresses:
+      {renderFields(index => (
+        <FormScope name={index}>
+          <div>
+            {`#${index + 1}`}
+            <div>
+              Street:
+              <Input name="street" />
+            </div>
+            <div>
+              City:
+              <Input name="city" />
+            </div>
+            <button type="button" onClick={() => remove(index)}>
+              Remove
+            </button>
+          </div>
+        </FormScope>
+      ))}
+      <button type="button" onClick={() => append()}>Add row</button>
+    </div>
+  );
+};
 
 const App = () => {
   const [value, setValue] = useState();
@@ -9,7 +39,23 @@ const App = () => {
   return (
     <div>
       <Form onSubmit={setValue}>
-        <Input type="text" name="name" />
+        <div>
+          First name:
+          <Input type="text" name="firstName" />
+        </div>
+        <div>
+          Last name:
+          <Input type="text" name="lastName" />
+        </div>
+        <FormScope name="address">
+          <div>
+            Addresses:
+            <FieldArray name="addresses">
+              <Addresses />
+            </FieldArray>
+          </div>
+
+        </FormScope>
         <input type="submit" value="Submit" />
       </Form>
       Submitted value:
